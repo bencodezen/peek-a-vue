f3cd6cb9e73589a08714c310b952b710e0e56cf7
Ben Hong <ben@bencodezen.io>
2020-10-25 19:24:27 -0400

    feat: create game board with cards

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
 <template>
   <h1>Peek-a-Vue</h1>
+  <section class="game-board">
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+    <div class="card"></div>
+  </section>
 </template>
 
 <script>
@@ -16,5 +34,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(4, 100px);
+  grid-template-rows: repeat(4, 100px);
+  grid-row-gap: 30px;
+  grid-column-gap: 30px;
+  justify-content: center;
+}
+
+.card {
+  border: 5px solid #ccc;
 }
 </style>
