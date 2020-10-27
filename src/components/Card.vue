944141334bd59cb33cc66fd8edd52d11869902d4
Ben Hong <ben@bencodezen.io>
2020-10-27 16:51:59 -0400

    feat: add flip animation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,6 @@
 <script>
+import { computed } from 'vue'
+
 export default {
   props: {
     matched: {
@@ -19,6 +21,12 @@ export default {
     }
   },
   setup(props, context) {
+    const flippedStyles = computed(() => {
+      if (props.visible) {
+        return 'is-flipped'
+      }
+    })
+
     const selectCard = () => {
       context.emit('select-card', {
         position: props.position,
@@ -27,6 +35,7 @@ export default {
     }
 
     return {
+      flippedStyles,
       selectCard
     }
   }
@@ -34,18 +43,24 @@ export default {
 </script>
 
 <template>
-  <div class="card" @click="selectCard">
-    <div v-if="visible" class="card-face is-front">
+  <div class="card" :class="flippedStyles" @click="selectCard">
+    <div class="card-face is-front">
       <img :src="`/images/${value}.png`" :alt="value" />
       <img v-if="matched" src="/images/checkmark.svg" class="icon-checkmark" />
     </div>
-    <div v-else class="card-face is-back"></div>
+    <div class="card-face is-back"></div>
   </div>
 </template>
 
 <style>
 .card {
   position: relative;
+  transition: 0.5s transform ease-in;
+  transform-style: preserve-3d;
+}
+
+.card.is-flipped {
+  transform: rotateY(180deg);
 }
 
 .card-face {
@@ -56,11 +71,13 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  backface-visibility: hidden;
 }
 
 .card-face.is-front {
   background-image: url('/images/card-bg.png');
   color: white;
+  transform: rotateY(180deg);
 }
 
 .card-face.is-back {
