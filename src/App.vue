418103a0b416f15a23fa019963de5f008d260ccf
Ben Hong <ben@bencodezen.io>
2020-10-26 20:45:21 -0400

    refactor: create card component with value prop

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <h1>Peek-a-Vue</h1>
   <section class="game-board">
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
-    <div class="card"></div>
+    <Card
+      v-for="(card, index) in cardList"
+      :key="`card-${index}`"
+      :value="card"
+    />
   </section>
 </template>
 
 <script>
+import Card from './components/Card'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Card
+  },
+  setup() {
+    const cardList = []
+
+    for (let i = 0; i < 16; i++) {
+      cardList.push(i)
+    }
+
+    return {
+      cardList
+    }
+  }
 }
 </script>
 
---
+++ src/components/Card.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="card">
+    {{ value }}
+  </div>
+</template>
+
+<style></style>
