764440789b31f51239d8614cb0418ab901901c0a
Ben Hong <ben@bencodezen.io>
2020-10-27 10:51:07 -0400

    feat: add win condition and track remaining pairs

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import { ref, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import Card from './components/Card'
 
 export default {
@@ -26,11 +26,26 @@ export default {
   setup() {
     const cardList = ref([])
     const userSelection = ref([])
-    const status = ref('')
+
+    const status = computed(() => {
+      if (remainingPairs.value === 0) {
+        return 'Player wins!'
+      } else {
+        return `Remaining Pairs: ${remainingPairs.value}`
+      }
+    })
+
+    const remainingPairs = computed(() => {
+      const remainingCards = cardList.value.filter(
+        card => card.matched === false
+      ).length
+
+      return remainingCards / 2
+    })
 
     for (let i = 0; i < 16; i++) {
       cardList.value.push({
-        value: i,
+        value: 8,
         visible: false,
         position: i,
         matched: false
