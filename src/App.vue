ec100227a7ae682d5e029a805175771d10b66a98
Ben Hong <ben@bencodezen.io>
2020-10-27 22:50:44 -0400

    feat: shuffle card animation

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <h1 class="sr-only">Peek-a-Vue</h1>
   <img src="/images/peek-a-vue-title.png" alt="Peek-a-Vue" class="title" />
-  <section class="game-board">
+  <transition-group tag="section" class="game-board" name="shuffle-card">
     <Card
-      v-for="(card, index) in cardList"
-      :key="`card-${index}`"
+      v-for="card in cardList"
+      :key="`${card.value}-${card.variant}`"
       :matched="card.matched"
       :value="card.value"
       :visible="card.visible"
       :position="card.position"
       @select-card="flipCard"
     />
-  </section>
+  </transition-group>
   <h2>{{ status }}</h2>
   <button @click="restartGame" class="button">
     <img src="/images/restart.svg" alt="Restart Icon" /> Restart Game
@@ -48,12 +48,8 @@ export default {
       return remainingCards / 2
     })
 
-    const shuffleCards = () => {
-      cardList.value = _.shuffle(cardList.value)
-    }
-
     const restartGame = () => {
-      shuffleCards()
+      cardList.value = _.shuffle(cardList.value)
 
       cardList.value = cardList.value.map((card, index) => {
         return {
@@ -79,6 +75,7 @@ export default {
     cardItems.forEach(item => {
       cardList.value.push({
         value: item,
+        variant: 1,
         visible: false,
         position: null,
         matched: false
@@ -86,6 +83,7 @@ export default {
 
       cardList.value.push({
         value: item,
+        variant: 2,
         visible: false,
         position: null,
         matched: false
@@ -144,7 +142,6 @@ export default {
       flipCard,
       userSelection,
       status,
-      shuffleCards,
       restartGame
     }
   }
@@ -212,5 +209,9 @@ h1 {
 
 .title {
   padding-bottom: 30px;
+}
+
+.shuffle-card-move {
+  transition: transform 0.8s ease-in;
 }
 </style>
