a97aaa69c38e3c6a134e11a63173bd56436d09e5
Ben Hong <ben@bencodezen.io>
2022-10-27 14:56:41 -0700

    refactor: move new game button to script setup

--- src/components/NewGameButton.vue
+++ src/components/NewGameButton.vue
@@ -1,21 +1,16 @@
-<script>
-export default {
-  props: {
-    newPlayer: {
-      type: Boolean,
-      default: true
-    }
-  },
-  setup(props, ctx) {
-    const startNewGame = () => {
-      ctx.emit('start-new-game')
-    }
-
-    return {
-      startNewGame
-    }
+<script setup>
+defineProps({
+  newPlayer: {
+    type: Boolean,
+    default: true
   }
-}
+});
+
+const emits = defineEmits(["start-new-game"]);
+
+const startNewGame = () => {
+  emits("start-new-game");
+};
 </script>
 
 <template>
@@ -48,7 +43,7 @@ export default {
   justify-content: center;
   margin: 0 auto 30px;
   font-weight: bold;
-  font-family: 'Titillium Web', sans-serif;
+  font-family: "Titillium Web", sans-serif;
   font-weight: bold;
   font-size: 1rem;
   border: 0;
